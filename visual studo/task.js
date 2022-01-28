ec1123a2a74d0119d458e316f3df7ad7a0d09f2c
HARI VENKATESAN <harideepak2511@gmail.com>
2022-01-28 21:14:47 +0530

    Add files via upload

---
+++ visual studo/task.js
@@ -0,0 +1,9 @@
+const a=["dog","cat","cow","goat","tiger","lion","rabbit"]
+const b=a.filter((e)=>e.length==3)
+console.log(b,b.length)
+const c=a.filter((e)=>e.length==4)
+console.log(c,c.length)
+const d=a.filter((e)=>e.length==5)
+console.log(d,d.length)
+const g=a.filter((e)=>e.length==6)
+console.log(g,g.length)
